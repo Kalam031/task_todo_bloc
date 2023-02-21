0d6a768abe6e0248ee73d03744984b32b120331e
Kalam <abukalam973@gmail.com>
2023-02-21 21:27:43 +0600

    update task event implemlentation

--- lib/blocs/bloc/tasks_bloc.dart
+++ lib/blocs/bloc/tasks_bloc.dart
@@ -19,6 +19,17 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
     ));
   }
 
-  void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {}
+  void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    final task = event.task;
+
+    List<Task> allTasks = List.from(state.allTask)..remove(task);
+    task.isDone == false
+        ? allTasks.add(task.copyWith(isDone: true))
+        : allTasks.add(task.copyWith(isDone: false));
+
+    emit(TasksState(allTask: allTasks));
+  }
+
   void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {}
 }
