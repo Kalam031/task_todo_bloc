f833b27fd9451d8aeb3d27a4a77fe7afca5cedd8
Kalam <abukalam973@gmail.com>
2023-02-23 20:57:40 +0600

    theme change in bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tasks_app/services/app_router.dart';
+import 'package:flutter_tasks_app/services/app_themes.dart';
 import 'package:path_provider/path_provider.dart';
 import '../blocs/my_bloc_observer.dart';
 import 'blocs/bloc_exports.dart';
@@ -31,14 +32,18 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => TasksBloc()),
         BlocProvider(create: (context) => SwitchBloc()),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Tasks App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const TasksScreen(),
-        onGenerateRoute: appRouter.onGenerateRoute,
+      child: BlocBuilder<SwitchBloc, SwitchState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Tasks App',
+            theme: state.switchValue
+                ? AppThemes.appThemeData[AppTheme.darkTheme]
+                : AppThemes.appThemeData[AppTheme.lightTheme],
+            home: const TasksScreen(),
+            onGenerateRoute: appRouter.onGenerateRoute,
+          );
+        },
       ),
     );
   }
--- lib/screens/my_drawer.dart
+++ lib/screens/my_drawer.dart
@@ -5,15 +5,8 @@ import '../screens/tasks_screen.dart';
 
 import '../blocs/bloc_exports.dart';
 
-class MyDrawer extends StatefulWidget {
+class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
-
-  @override
-  State<MyDrawer> createState() => _MyDrawerState();
-}
-
-class _MyDrawerState extends State<MyDrawer> {
-  bool switchValue = false;
 
   @override
   Widget build(BuildContext context) {
@@ -59,13 +52,17 @@ class _MyDrawerState extends State<MyDrawer> {
               );
             },
           ),
-          Switch(
-              value: switchValue,
-              onChanged: (newValue) {
-                setState(() {
-                  switchValue = newValue;
-                });
-              })
+          BlocBuilder<SwitchBloc, SwitchState>(
+            builder: (context, state) {
+              return Switch(
+                  value: state.switchValue,
+                  onChanged: (newValue) {
+                    newValue
+                        ? context.read<SwitchBloc>().add(SwitchOnEvent())
+                        : context.read<SwitchBloc>().add(SwitchOffEvent());
+                  });
+            },
+          )
         ],
       ),
     );
